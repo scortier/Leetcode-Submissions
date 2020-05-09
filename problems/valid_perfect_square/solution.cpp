63ea0991932df21970d5ede5964eab294a794c30
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-05-09 07:22:04 +0000

    Sync LeetCode submission - Valid Perfect Square (cpp)

---
+++ problems/valid_perfect_square/solution.cpp
@@ -0,0 +1,19 @@
+// defining class
+
+class Solution {
+public:
+    // defining
+    bool isPerfectSquare(int num) {
+        long int i = 1;
+        for(i = 1;i*i < num;i++){
+            
+        }
+        // logical execution
+        if(i*i == num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
