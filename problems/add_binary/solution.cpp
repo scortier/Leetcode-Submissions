d737b671f3ce3b0fa9629f973953296da06961f9
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-07-19 16:26:38 +0000

    Sync LeetCode submission - Add Binary (cpp)

---
+++ problems/add_binary/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+        int i=a.length()-1;
+      int j=b.length()-1;
+      int c=0;
+      while(i>=0 || j>=0)
+      {
+        int sum=c;
+        if(i>=0) sum+=a[i--]-'0';
+        if(j>=0) sum+=b[j--]-'0';
+        c=sum>1?1:0;
+        res+=to_string(sum%2);
+      }
+      if(c) res+=to_string(c);
+      reverse(res.begin(),res.end());    
+      return res;
+    }
+};
