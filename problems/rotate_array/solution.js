f204e7848e2d2d9ad9ffe283d56f2099689f5731
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-10-15 09:59:48 +0000

    Sync LeetCode submission - Rotate Array (javascript)

---
+++ problems/rotate_array/solution.js
@@ -0,0 +1,14 @@
+var rotate = function(nums, k) { // 10 mins
+    k = k%nums.length;
+    reverse(0, nums.length-1);
+    reverse(0, k-1);
+    reverse(k, nums.length-1);
+
+
+    function reverse(i, j){
+        while(i<j){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++; j--;
+        }
+    }
+}; 
