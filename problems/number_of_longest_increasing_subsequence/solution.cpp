08a292131e3599d76a0538e79efbe70c7a2d85fd
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-10-30 07:34:46 +0000

    Sync LeetCode submission - Number of Longest Increasing Subsequence (cpp)

---
+++ problems/number_of_longest_increasing_subsequence/solution.cpp
@@ -0,0 +1,20 @@
+// O(n^2) time, O(n) space
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size(), maxlen = 0, res = 0;
+        vector<int> dp(n,1), count(n,1);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    if(dp[i] < dp[j] + 1)   dp[i] = dp[j] + 1, count[i] = count[j];
+                    else if(dp[i] == dp[j] + 1)     count[i] += count[j];
+                }
+            }
+            if(maxlen == dp[i])     res += count[i];
+            else if(maxlen < dp[i])     maxlen = dp[i], res = count[i];
+        }
+        return res;
+    }
+};
