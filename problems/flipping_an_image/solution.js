eb882e4f6141094da63d6e00b46af0413a79bba3
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-11-10 15:07:47 +0000

    Sync LeetCode submission - Flipping an Image (javascript)

---
+++ problems/flipping_an_image/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(A) {
+    return new Array(A.length).fill(0)
+    .map((_, r) => A[r].map((_, c) => ~A[r][A[0].length - c - 1] & 1));
+}; 
