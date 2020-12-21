182fa595deaa37c7456d6abafde2aa1062e73438
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-12-21 15:09:42 +0000

    Sync LeetCode submission - Smallest Range II (cpp)

---
+++ problems/smallest_range_ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int smallestRangeII(vector<int>& A, int K) {
+    sort(A.begin(), A.end());
+
+    int n = A.size();
+
+    int ans = A[n - 1] - A[0];
+
+    for (int i = 0; i < n - 1; ++i) {
+      int mx = max(A[i] + K, A[n - 1] - K);
+      int mn = min(A[i + 1] - K, A[0] + K);
+
+      ans = min(ans, mx - mn);
+    }
+
+    return ans;
+  }
+};
