be724eb292588cf26ed8eb10d19fcc826750a265
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-01-01 15:11:53 +0000

    Sync LeetCode submission - Check Array Formation Through Concatenation (cpp)

--- problems/check_array_formation_through_concatenation/solution.cpp
+++ problems/check_array_formation_through_concatenation/solution.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
-        int n = arr.size();
-        int m = pieces.size();
-        int i = 0;
-        
-        while(i < n){
-            int prev = i;
-            int found = false;
-            for(int j=0; j<m; j++){
-                if(!found && pieces[j][0] == arr[i]){
-                    i++;
-                    found = true;
-                    for(int k=1; k<pieces[j].size(); k++){
-                        if(pieces[j][k] == arr[i]){
-                            i++;
-                        }
-                        else
-                            return false;
-                    }
-                }
+        // map first element of each piece to its position in pieces
+        unordered_map<int, int> mp;
+        for(int i = 0; i < pieces.size(); i++)  mp[pieces[i].front()] = i;
+
+        // check if we can form arr using pieces
+        int j = 0;
+        while(j < arr.size()) {
+            if(!mp.count(arr[j]))   return false;
+
+            for(auto x: pieces[mp[arr[j]]]) {
+                if(x != arr[j]) return false;
+                j++;
             }
-            if(i == prev)
-                return false;
-            
         }
         return true;
-        
-        
     }
 };
