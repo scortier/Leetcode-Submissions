d7f5e7607f8827c706e31765f3483ad6d787b6d4
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-02-21 14:02:46 +0000

    Sync LeetCode submission - Broken Calculator (cpp)

---
+++ problems/broken_calculator/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int c=0;
+
+        while(Y>X){
+            if(Y%2)Y++;
+            else Y/=2;
+            c++;
+        }
+        c+=X-Y;
+        return c;
+    }
+};
