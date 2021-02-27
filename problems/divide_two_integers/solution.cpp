056d57426880f6be2bf284b8d0c25cf5a725391e
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-02-27 11:36:34 +0000

    Sync LeetCode submission - Divide Two Integers (cpp)

--- problems/divide_two_integers/solution.cpp
+++ problems/divide_two_integers/solution.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    pair<int,int> di(int dividend,int divisor){
-        if(dividend<divisor) return {dividend,0};
-        pair<int,int> a= di(dividend>>1,divisor);
-        dividend=(dividend & 1) + (a.first<<1) ;
-        if(dividend<divisor) return{dividend,a.second<<1};
-        return {dividend-divisor,(a.second<<1)+1};
-    }
     int divide(int dividend, int divisor) {
-        if(divisor==INT_MIN) return dividend==INT_MIN?1:0;
-        if(dividend==INT_MIN ) {
-            if(divisor==1)return INT_MIN;
-            if (divisor==-1) return INT_MAX;
-            return (((dividend<0)^(divisor<0))?-1:1)*(di(abs(dividend+abs(divisor)),abs(divisor)).second+1);
+        if(dividend == INT_MIN and divisor == -1) return INT_MAX;
+        if(dividend == INT_MIN and divisor == 1) return INT_MIN;
+
+        bool negative = (dividend < 0) ^ (divisor < 0);
+
+        dividend = dividend > 0 ? -dividend : dividend;
+        divisor = divisor > 0 ? -divisor : divisor;
+
+        cout << dividend << " " << divisor << "\n";
+        int quotient = 0;
+        while(dividend <= divisor){
+            int i = 1, accum = divisor;
+            while(accum > INT_MIN >> 1 and dividend <= accum + accum){
+                i += i;
+                accum += accum;
+            }
+
+            dividend -= accum;
+            quotient += i;
         }
-        
-        return ((((dividend<0)^(divisor<0))?-1:1)*di(abs(dividend),abs(divisor)).second);
+
+        return negative ? -quotient : quotient;
     }
 };
