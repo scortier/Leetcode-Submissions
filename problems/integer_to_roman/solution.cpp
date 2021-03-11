6c7103ce71015bd80bee4eed988022a96451ffcb
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-03-11 00:49:53 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

--- problems/integer_to_roman/solution.cpp
+++ problems/integer_to_roman/solution.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        string I[10] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        string X[10] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        string C[10] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        string M[10] = {"", "M", "MM", "MMM"};
-        return M[num / 1000] + C[num / 100 % 10] + X[num / 10 % 10] + I[num % 10];
+     string intToRoman(int num)
+    {       
+        string res = "";
+        string s[] = {"I", "X", "C", "M", "V", "L", "D", ""};
+        int i = 0;
+        while (num)
+        {
+            switch(num % 10)
+            {
+                case 1: res.insert(0, s[i]); break;
+                case 2: res.insert(0, s[i] + s[i]); break;
+                case 3: res.insert(0, s[i] + s[i] + s[i]); break;
+                case 4: res.insert(0, s[i] + s[i + 4]); break;
+                case 5: res.insert(0, s[i + 4]); break;
+                case 6: res.insert(0, s[i + 4] + s[i]); break;
+                case 7: res.insert(0, s[i + 4] + s[i] + s[i]); break;
+                case 8: res.insert(0, s[i + 4] + s[i] + s[i] + s[i]); break;
+                case 9: res.insert(0, s[i] + s[i + 1]); break;  
+            }
+            num /= 10;
+            i++;
+        }
+        return res;
     }
 };
