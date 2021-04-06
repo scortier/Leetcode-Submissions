3866cceceeded029b00e3be54339380012484b73
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-04-06 11:12:24 +0000

    Sync LeetCode submission - Minimum Operations to Make Array Equal (cpp)

--- problems/minimum_operations_to_make_array_equal/solution.cpp
+++ problems/minimum_operations_to_make_array_equal/solution.cpp
@@ -1,47 +1,11 @@
 class Solution {
 public:
     int minOperations(int n) {
-        
-        // new int
-        
-        int target;
-        
-        // looping
-        
-        
-        // logic
-        
-        if (n % 2 == 1) target = 2 * (n / 2) + 1;
-        
-        
-        // next cond
-        
-        else {
-            int dividend = 2 * (n/2 - 1) + 1 + 2 * (n / 2) + 1;
-            target = dividend / 2;
+        int ans = 0;
+        if(n&1) {
+            n/=2; return n*(n+1);
         }
-        
-        // result init zero
-        
-
-        int res = 0;
-        
-        // run till half
-        
-
-        for (int i = 0; i < n/2; i++){
-            
-            // logic of answer
-            
-            res += abs( target - (2*i + 1) );
-        }
-        
-        // answer
-        
-
-        return res;
+        n/=2;
+        return n*n;
     }
-    
-    // end 
-    
 };
