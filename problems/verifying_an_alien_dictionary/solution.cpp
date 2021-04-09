6b49c59bcfd1b335850e973d2eb700292ec68ac7
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-04-09 11:01:04 +0000

    Sync LeetCode submission - Verifying an Alien Dictionary (cpp)

---
+++ problems/verifying_an_alien_dictionary/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char , int> mp;
+        for(int i=0;i<26;i++)
+            mp[order[i]] = i;
+        for(int i=0;i<words.size();i++)
+        {
+            for(auto &c : words[i])
+            {
+                c = mp[c] + 'a';
+            }
+
+            if(i>0)
+            {
+                if(words[i-1]>words[i]) return 0;
+            }
+        }
+        return 1;
+
+    }
+};
