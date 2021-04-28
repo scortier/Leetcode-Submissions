67a42a2415b8bd49dbb7c69375b71c8eac47785c
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-04-28 10:37:57 +0000

    Sync LeetCode submission - Shortest Unsorted Continuous Subarray (cpp)

--- problems/shortest_unsorted_continuous_subarray/solution.cpp
+++ problems/shortest_unsorted_continuous_subarray/solution.cpp
@@ -1,60 +1,37 @@
-class Solution 
+class Solution
 {
 public:
-    int findUnsortedSubarray(vector<int>& nums) 
+    int findUnsortedSubarray(vector<int> &nums)
     {
-        int l = 0;
-        int r = nums.size() - 1;
-        while (l < nums.size() - 1)
+        int n = nums.size();
+        if (n == 1)
         {
-            if (nums[l] > nums[l + 1])
+            return 0;
+        }
+        vector<int> newNums = nums;
+        sort(newNums.begin(), newNums.end());
+        int i = 0, j = n - 1;
+
+        while (i < j)
+        {
+            if (nums[i] == newNums[i])
+            {
+                i++;
+            }
+
+            if (nums[j] == newNums[j])
+            {
+                j--;
+            }
+
+            if (nums[i] != newNums[i] && nums[j] != newNums[j])
             {
                 break;
             }
-            l++;
         }
-
-        while (r > l)
-        {
-            if (nums[r] < nums[r - 1])
-            {
-                break;
-            }
-            r--;
-        }
-
-        if (r == l) return 0;
-
-        int minval = INT_MAX;
-        int maxval = INT_MIN;
-        int i = l;
-        while (i <= r)
-        {
-            if (nums[i] < minval)
-            {
-                minval = nums[i];
-            }
-
-            if (nums[i] > maxval)
-            {
-                maxval = nums[i];
-            }
-
-            i++;
-        }
-
-        while (l >= 0 && nums[l] > minval)
-        {
-            l--;
-        }
-        l++;
-
-        while (r < nums.size() && nums[r] < maxval)
-        {
-            r++;
-        }
-        r--;
-
-        return r - l + 1; 
+        if (i == j)
+            return 0;
+        else
+            return (j - i + 1);
     }
 };
